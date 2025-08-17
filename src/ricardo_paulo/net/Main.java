3bb2057f8695558befd2199705ebca33e369413e
Paulo Ricardo <pauloricardo@pop-os.tail41648.ts.net>
2025-08-17 13:32:03 -0300

    Startup Estrutura de pastas criada e .gitignore criado

---
+++ src/ricardo_paulo/net/Main.java
@@ -0,0 +1,7 @@
+package ricardo_paulo.net;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Escolha uma branch para visualizar uma atividade.");
+    }
+}
